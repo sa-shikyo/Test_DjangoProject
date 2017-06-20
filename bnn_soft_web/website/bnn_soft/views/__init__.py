e91cd7a5ae07e24a9d8ccbf59e27071053dfbe35
sa-shikyo <chzuozhiqiang@sohu.com>
2017-06-20 21:16:17 +0800

    修正

--- bnn_soft_web/website/bnn_soft/views/__init__.py
+++ bnn_soft_web/website/bnn_soft/views/__init__.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from bnn_soft.views.bnn_soft import *
+from bnn_soft.views.bnn_soft import bnn_homepage
+from bnn_soft.views.bnn_soft import bnn_company
+from bnn_soft.views.bnn_soft import aaaa
+from bnn_soft.views.bnn_soft import bbbb
+from bnn_soft.views.bnn_soft import cccc
