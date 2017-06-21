9d0918541417ddbab517b48f6ea1c77ec99b421c
s38922 <sa-shikyou@intage.co.jp>
2017-06-21 16:30:08 +0900

    修正

--- bnn_soft_web/website/bnn_soft/views/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from views.bnn_soft import bnn_homepage
-from views.bnn_soft import bnn_company
-from views.bnn_soft import aaaa
-from views.bnn_soft import bbbb
-from views.bnn_soft import cccc
---
+++ test_web/website/test/views/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from test.views.test_action import *
